de91892d656aebe569b6182e74f67117acfacdb3
wty <116503270+DaemonHook@users.noreply.github.com>
2024-08-15 22:00:47 +0800

    update

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="dashboard-container">
+    <!-- <component :is="currentRole" /> -->
+    <h2>Dashboard</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      currentRole: 'adminDashboard'
+    }
+  },
+}
+</script>
